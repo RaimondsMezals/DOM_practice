e84216ccd3f7b0be6fcd9c76b8bb8c7ee8cb39e5
RaimondsMezalsQA <RMezals@qa.com>
2021-02-04 11:55:51 +0000

    Added event listener

--- main.js
+++ main.js
@@ -7,10 +7,19 @@ let task1 = () => {
     let heading1 = document.createElement("h1");
     heading1.innerText = "This is my h1 heading!";
     bdy.appendChild(heading1);
+    test();
+    butt1.removeEventListener("click", task1);
 }
 
-task1();
+let test = () => {
+console.log("hey");
+
+}
 
 let alt1 = document.querySelector("#one");
 console.log(alt1);
 alt1.innerText = "New Text!";
+
+let butt1 = document.querySelector("#buttOne");
+
+butt1.addEventListener("click", task1);
